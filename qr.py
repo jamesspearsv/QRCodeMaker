93f3ee935ff8019832e7da4606e85b98a5bed6dc
jamesspearsv <jamesspearsv@gmail.com>
2020-12-26 11:47:56 -0500

    Addec qr.py

---
+++ qr.py
@@ -0,0 +1,13 @@
+from tkinter import *
+#import qrcode
+
+app = Tk()
+app.title("QR Code Maker")
+
+app.mainloop()
+
+#url = input("URL: ")
+#fileName = input("File name?: ")
+
+#img = qrcode.make(url)
+#img.save(f"{fileName}.png", "PNG")
